dd8470e7129a8a9acbee48accfa6968c0acd65a5
lunguinhoantonio <antoniolunguinho007@gmail.com>
2025-02-18 18:08:28 -0300

    feat: add sound configuration system

--- js/script.js
+++ js/script.js
@@ -2,14 +2,53 @@ const mario = document.querySelector('.mario');
 const pipe = document.querySelector('.pipe');
 const clouds = document.querySelector('.clouds');
 
+const openPopup = document.getElementById("config");
+const closePopup = document.getElementById("closePopup");
+const popupOverlay = document.getElementById("popupOverlay");
+
+
+openPopup.addEventListener("click", () => {
+    popupOverlay.style.display = "block";
+});
+
+closePopup.addEventListener("click", () => {
+    popupOverlay.style.display = "none";
+});
+
+popupOverlay.addEventListener("click", (event) => {
+    if (event.target === popupOverlay) {
+        popupOverlay.style.display = "none";
+    }
+});
+
+
+const jumpSound = new Audio("./assets/jump.mp3");
 const gameMusic = new Audio('./assets/music-theme.mp3');
 gameMusic.play();
 const gameOverMusic = new Audio('./assets/die.mp3');
 
+const toggleMusic = document.getElementById("musicToggle");
+function checkSwitches() {
+    if (toggleMusic.checked) {
+        gameMusic.currentTime = 0;
+        gameMusic.volume = 100;
+    } else {
+        gameMusic.volume = 0;
+    }
+}
+const toggleSoundEffects = document.getElementById("soundEffectsToggle");
+
+
+musicToggle.addEventListener("change", checkSwitches);
+checkSwitches();
+
 const jump = () => {
     mario.classList.add('jump');
-    const jumpSound = new Audio("./assets/jump.mp3");
-    jumpSound.play();
+    if (soundEffectsToggle.checked) {
+        jumpSound.play();
+    } else {
+        jumpSound.pause();
+    }
     setTimeout(() => {
         mario.classList.remove('jump');
     }, 500);
@@ -31,9 +70,12 @@ const loop = setInterval(() => {
 
     if (pipePosition <= 100 && pipePosition > 0 && marioPosition < 80) {
         const cloudPosition = getComputedStyle(clouds).right.replace('px', '');
-        
-        gameMusic.pause();
-        gameOverMusic.play();
+        gameMusic.volume = 0;
+        if (toggleMusic.checked) {
+            gameOverMusic.play();
+        } else {
+            gameOverMusic.pause();
+        }
 
         clouds.style.animation = 'none';
         clouds.style.right = `${cloudPosition}px`;
@@ -45,8 +87,8 @@ const loop = setInterval(() => {
         mario.style.bottom = `${marioPosition}px`;
 
         mario.src = './assets/game-over.png';
-        mario.style.width = '75px'
-        mario.style.marginLeft = '30px'
+        mario.style.width = '75px';
+        mario.style.marginLeft = '30px';
 
         const gameOverButton = document.createElement('button');
         gameOverButton.classList.add('game-over-button');
@@ -66,3 +108,21 @@ const loop = setInterval(() => {
 }, 10);
 
 document.addEventListener('keydown', jump);
+
+function saveSwitchState() {
+    localStorage.setItem("musicEnabled", toggleMusic.checked);
+    localStorage.setItem("soundEffectsEnabled", toggleSoundEffects.checked);
+}
+
+function loadSwitchState() {
+    const musicState = localStorage.getItem("musicEnabled") === "true";
+    const soundEffectsState = localStorage.getItem("soundEffectsEnabled") === "true";
+
+    toggleMusic.checked = musicState;
+    toggleSoundEffects.checked = soundEffectsState;
+}
+
+musicToggle.addEventListener("change", saveSwitchState);
+soundEffectsToggle.addEventListener("change", saveSwitchState);
+
+loadSwitchState();
